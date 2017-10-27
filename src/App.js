2d148a5b5d06239e0a5ef6d6a95cd0f7fe537541
Alex Wilson <acwilson96@protonmail.com>
2017-10-27 11:30:24 +0100

    Set up exporting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Home from './Views/Home.js';
+import Home from './Views/';
 
 class App extends Component {
   render() {
--- src/Views/Home/index.js
+++ src/Views/Home/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Navbar } from '../Components';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
+        <Navbar />
           Homepage
       </div>
     );
