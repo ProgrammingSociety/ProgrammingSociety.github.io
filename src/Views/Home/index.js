cd5e0a210ca4d5671438cbc428f72927ad1ea49a
Alex Wilson <acwilson96@protonmail.com>
2017-12-21 21:38:34 +0000

    Homepage

--- src/Views/Home/index.js
+++ src/Views/Home/index.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
-import { Navbar } from '../../Components';
+// import { Navbar } from '../../Components';
 import './styles.css';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <Navbar />
-        <div className="body">
-          <div className="heading">
-            About the Society
-          </div>
+        <div className="centered">
+          <img src="/logo.png" alt="Programming Society Logo" height="500px" />
+        </div>
+        <div className="centered">
+          <a href="/about" className="home-link">About</a>
+          <a href="/events" className="home-link">Events</a>
+          <a href="/members" className="home-link">Members</a>
+          <a href="/resources" className="home-link">Resources</a>
+          <a href="/contact" className="home-link">Contact</a>
         </div>
       </div>
     );
