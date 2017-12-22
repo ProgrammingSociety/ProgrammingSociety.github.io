9c7ece72d9ef4b58dcb7170ee797ec3837f9b6b7
Alex Wilson <acwilson96@protonmail.com>
2017-12-22 16:40:04 +0000

    Fix home background colour

--- src/Views/Home/index.js
+++ src/Views/Home/index.js
@@ -5,7 +5,7 @@ import './styles.css';
 export default class Home extends Component {
   render() {
     return (
-      <div>
+      <div className="home-container">
         <div className="centered">
           <img src="/logo.png" alt="Programming Society Logo" height="500px" />
         </div>
