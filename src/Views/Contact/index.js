e7c14eb2223dbe04c7a62d353eeb59f490be3169
Alex Wilson <acwilson96@protonmail.com>
2017-12-22 20:05:57 +0000

    Contact page pending review

--- src/Views/Contact/index.js
+++ src/Views/Contact/index.js
@@ -7,9 +7,37 @@ export default class Contact extends Component {
     return (
       <div>
         <Navbar />
-        <div className="body">
-          <div className="heading">
-            Contact Programming Society
+        <div className="contact-page-body">
+          <div className="email-facebook">
+            <a href="www.facebook.com/pg/programmingsociety">www.facebook.com/pg/programmingsociety</a>
+            <br />
+            ediprogsoc@gmail.com
+          </div>
+          <div className="contact-heading">
+            Committee 2017/2018
+          </div>
+          <div className="contact-body">
+            <p>
+            <strong>President</strong> <br />
+            Jennifer Za Nzambi <br />
+            elvira.thea@yahoo.co.uk <br /> <br />
+
+            <strong>Secretary</strong> <br />
+            Kristyna Duongova <br />
+            kristynaduong94@gmail.com <br /> <br />
+
+            <strong>Head of Teaching</strong> <br />
+            Jirka Vebr <br />
+            jirkavebr@gmail.com <br /> <br /> <br />
+
+
+            <strong>Tutors</strong> <br />
+            Martin Svoboda &emsp;visgean@gmail.com <br />
+            Petr Manas&emsp;&emsp;&emsp; s1652610@ed.ac.uk <br />
+            Dylan Oshima&emsp;&emsp;s1764822@ed.ac.uk <br />
+            Argyris Zardilis &emsp; azardilis@gmail.com <br />
+
+            </p>
           </div>
         </div>
       </div>
