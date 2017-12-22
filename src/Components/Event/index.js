dae6776e4e0b02c4263dd9300b717e80381c81c9
Alex Wilson <acwilson96@protonmail.com>
2017-12-22 19:52:01 +0000

    Export the Event Component

---
+++ src/Components/Event/index.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+export default class Event extends Component {
+
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  dateToString = (date) => {
+    switch (date[1]) {
+      case '01':
+        return 'JAN';
+      case '02':
+        return 'FEB';
+      case '09':
+        return 'SEP';
+      case '10':
+        return 'OCT';
+      case '11':
+        return 'NOV';
+      case '12':
+        return 'DEC';
+      default:
+        return 'N/A';
+    }
+  }
+
+  openEvent = () => {
+    window.location.href = this.props.facebook;
+  }
+
+  render() {
+
+    let dateArr       = this.props.date.split('/');
+    let formattedDate = dateArr[1] + '/' + dateArr[0] + '/' + dateArr[2];
+    let currDate      = new Date();
+    let eventDate     = new Date(formattedDate);
+    let dateClass     = 'date-middle';
+    if (currDate > eventDate) {
+      dateClass += ' event-finished';
+    }
+    else {
+      dateClass += ' event-upcoming';
+    }
+    
+    if (this.props.facebook != null) {
+      return (
+        <div className="event-container">
+          <div className="event-date">
+            <span className={dateClass}>
+              {dateArr[0]}<br/>{this.dateToString(dateArr)}
+            </span>
+          </div>
+          <div className="event-body">
+            <a className="event-title clickable" href={this.props.facebook}>{this.props.title}</a>
+            <div className="event-time">{this.props.time}</div>          
+            <div className="event-location">{this.props.location}</div>
+          </div>
+        </div>
+      );
+    }
+    else {
+      return (
+        <div className="event-container">
+          <div className="event-date">
+            <span className={dateClass}>
+              {dateArr[0]}<br/>{this.dateToString(dateArr)}
+            </span>
+          </div>
+          <div className="event-body">
+            <div className="event-title">{this.props.title}</div>
+            <div className="event-time">{this.props.time}</div>          
+            <div className="event-location">{this.props.location}</div>
+          </div>
+        </div>
+      );
+    }
+  }
+}
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,4 @@
 import Navbar from './Navbar';
+import Event from './Event';
 
-export { Navbar };
+export { Event, Navbar };
