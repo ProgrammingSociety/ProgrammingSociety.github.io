9c24c8020dfdee9199474f27203a32c9d2d7fd2f
Alex Wilson <acwilson96@protonmail.com>
2018-01-24 11:56:44 +0000

    Add new event

--- src/Views/Events/index.js
+++ src/Views/Events/index.js
@@ -14,7 +14,7 @@ export default class Events extends Component {
           <div className="events-body">
             {/* <Event date="30/12/2017" time="17:30 - 19:00" title="Some Future Event" location="LG.10 - DHT" facebook="http://www.google.com" />          
             <Event date="22/12/2017" time="17:30 - 19:00" title="Some Future Event" location="LG.10 - DHT" />           */}
-            
+            <Event date="30/11/2017" time="17:00 - 19:00" title="Programming Refreshers' & Refreshment" location="LG.07 - DHT" />
             <Event date="30/11/2017" time="17:30 - 19:00" title="Pre-Christmas CMU" location="LG.10 - DHT" facebook="https://www.facebook.com/events/100880230699940/" />
             <Event date="23/11/2017" time="17:30 - 19:00" title="CMU IX - Tic Tac Toe" location="LG.10 - DHT" facebook="https://www.facebook.com/events/1948773472111318/" />
             <Event date="16/11/2017" time="17:30 - 19:00" title="CMU VIII - Tic Tac Toe" location="LG.10 - DHT" facebook="https://www.facebook.com/events/1948773472111318/" />
