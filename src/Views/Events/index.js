01a0f5c1947dcfda69e3365a654b51d6542f0a73
Alex Wilson <acwilson96@protonmail.com>
2018-01-24 19:09:27 +0000

    Added facebook URL to event

--- src/Views/Events/index.js
+++ src/Views/Events/index.js
@@ -22,7 +22,7 @@ export default class Events extends Component {
             <Event date="14/02/2018" time="17:00 - 19:00" title="CMU - TBD" location="LG.07 - DHT" />   
             <Event date="07/02/2018" time="17:00 - 19:00" title="Basic introduction to Machine Learning" location="LG.07 - DHT" />   
             <Event date="31/01/2018" time="17:00 - 19:00" title="Building a simulation of the Monty Hall Problem" location="LG.07 - DHT" />            
-            <Event date="24/01/2018" time="17:00 - 19:00" title="Programming Refreshers' & Refreshment" location="LG.07 - DHT" />
+            <Event date="24/01/2018" time="17:00 - 19:00" title="Programming Refreshers' & Refreshment" location="LG.07 - DHT" facebook="https://www.facebook.com/events/1650966814989911/" />
             <Event date="30/11/2017" time="17:30 - 19:00" title="Pre-Christmas CMU" location="LG.10 - DHT" facebook="https://www.facebook.com/events/100880230699940/" />
             <Event date="23/11/2017" time="17:30 - 19:00" title="CMU IX - Tic Tac Toe" location="LG.10 - DHT" facebook="https://www.facebook.com/events/1948773472111318/" />
             <Event date="16/11/2017" time="17:30 - 19:00" title="CMU VIII - Tic Tac Toe" location="LG.10 - DHT" facebook="https://www.facebook.com/events/1948773472111318/" />
