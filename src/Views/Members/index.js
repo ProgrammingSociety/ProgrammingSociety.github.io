e724a7495279a7c3c1c0ee01a76c3ddfd97e5e15
Kristynaduong <kristynaduong94@gmail.com>
2018-02-01 00:10:09 +0000

    Update members page

--- src/Views/Members/index.js
+++ src/Views/Members/index.js
@@ -9,12 +9,17 @@ export default class Members extends Component {
         <Navbar />
         <div className="members-page-body">
           <div className="members-heading">
-            Society Member Info
+            Society Members
           </div>
           <div className="members-body">
-            Coming Soon
-          </div>
-        </div>
+            The members of our society are mostly undergraduate and postgraduate students of various backgrounds from social sciences, engineering to law or graphic design. 
+      We understand that many students struggle to find the time to code in their free time.
+      Therefore, we would like to acknowledge the effort of those who come along regularly and put in the extra effort to learn beyond the scope of their degree.
+      
+      The following are the members who participated in majority of our sessions and received a ProgSoc T-shirt to commemorate their good work:
+      
+      
+   
       </div>
     );
   }
